159630c319faa22008247c3a6c8f41800a4acc9a
William Wu <dev.wwu@gmail.com>
2025-11-28 10:55:47 -0800

    Parse JavaScript file

--- packages/core/src/main.rs
+++ packages/core/src/main.rs
@@ -1,11 +1,28 @@
-use clap::Parser;
+use anyhow::{Context, Result};
+use clap::Parser as CliParser;
+use oxc_allocator::Allocator;
+use oxc_parser::Parser;
+use oxc_span::SourceType;
 
-#[derive(Parser)]
+#[derive(CliParser)]
 struct CliArguments {
     path: std::path::PathBuf,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let args = CliArguments::parse();
-    println!("path: {:?}", args.path);
+    let path = args.path;
+
+    let content = std::fs::read_to_string(&path)
+        .with_context(|| format!("Couldn't read file `{}`", path.display()))?;
+
+    let source_type =
+        SourceType::from_path(path).with_context(|| format!("Couldn't identify source type"))?;
+    let allocator = Allocator::default();
+    let parser_result = Parser::new(&allocator, &content, source_type).parse();
+    let program = parser_result.program;
+
+    println!("{:#?}", program);
+
+    Ok(())
 }
