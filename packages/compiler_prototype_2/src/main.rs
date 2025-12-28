221bc747101643e56a91cdec141b10a36cb43cfd
William Wu <dev.wwu@gmail.com>
2025-12-28 10:50:05 -0800

    Copy main function

--- packages/compiler_prototype_2/src/main.rs
+++ packages/compiler_prototype_2/src/main.rs
@@ -1,3 +1,82 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{Context, Result};
+use clap::Parser as CliParser;
+use oxc::{
+    allocator::Allocator, ast::ast::Program, parser::Parser, semantic::SemanticBuilder,
+    span::SourceType,
+};
+
+#[derive(CliParser)]
+struct CliArguments {
+    path: std::path::PathBuf,
+}
+
+fn main() -> Result<()> {
+    let args = CliArguments::parse();
+    let path = args.path;
+
+    let content = std::fs::read_to_string(&path)
+        .with_context(|| format!("Couldn't read file `{}`", path.display()))?;
+
+    let source_type =
+        SourceType::from_path(path).with_context(|| format!("Couldn't identify source type"))?;
+    let allocator = Allocator::default();
+    let parser_result = Parser::new(&allocator, &content, source_type).parse();
+    let program = parser_result.program;
+
+    if !parser_result.errors.is_empty() {
+        let error_messages = parser_result
+            .errors
+            .into_iter()
+            .map(|error| format!("{:?}", error.with_source_code(content.clone())))
+            .collect::<Vec<String>>()
+            .join("\n");
+        println!("Parse errors:\n{error_messages}");
+    }
+
+    let semantic_result = SemanticBuilder::new()
+        .with_check_syntax_error(true)
+        .build(&program);
+
+    if !semantic_result.errors.is_empty() {
+        let error_messages = semantic_result
+            .errors
+            .into_iter()
+            .map(|error| format!("{:?}", error.with_source_code(content.clone())))
+            .collect::<Vec<String>>()
+            .join("\n");
+        println!("Semantic errors:\n{error_messages}");
+    }
+
+    // println!("{:#?}", &program);
+
+    let data_pack = compile_program(program);
+    std::fs::create_dir_all("smelter_prototype/data/smelter/function")
+        .with_context(|| "Couldn't create directories")?;
+    for function in data_pack {
+        std::fs::write(
+            format!(
+                "smelter_prototype/data/smelter/function/{}.mcfunction",
+                &function.name
+            ),
+            function.body.join("\n"),
+        )
+        .with_context(|| format!("Couldn't write file `{}.mcfunction`", &function.name))?
+    }
+    std::fs::write(
+        "smelter_prototype/pack.mcmeta",
+        "{\"pack\":{\"description\":\"smelter prototype\",\"min_format\":[94,1],\"max_format\":[94,1]}}",
+    ).with_context(|| "Couldn't write file: `pack.mcmeta`")?;
+
+    Ok(())
+}
+
+struct Mcfunction {
+    name: String,
+    body: Vec<String>,
+}
+
+type DataPack = Vec<Mcfunction>;
+
+fn compile_program(program: Program) -> DataPack {
+    Vec::new()
 }
