63ceb4f2c816d4064adb95abea2bea660ecaef9f
William Wu <dev.wwu@gmail.com>
2026-03-22 17:48:15 -0700

    Compile trait

--- packages/compiler_prototype_3/src/smelting.rs
+++ packages/compiler_prototype_3/src/smelting.rs
@@ -1,3 +1,5 @@
+use crate::data_pack::DataPackBuilder;
+
 pub enum SmeltingLiteral {
     Boolean(bool),
     Integer(i32),
@@ -24,16 +26,22 @@ pub enum SmeltingOperation {
     StringConcatenation(Box<SmeltingExpression>, Box<SmeltingExpression>),
 }
 
+type ExpressionId = i32;
+
 pub enum SmeltingExpression {
-    Command(String),
-    FunctionCall(String, Vec<SmeltingExpression>),
-    ListElementAccess(Box<SmeltingExpression>, Box<SmeltingExpression>),
-    ListInitialization(Vec<SmeltingExpression>),
-    Literal(Box<SmeltingLiteral>),
-    Operation(Box<SmeltingOperation>),
-    StructInitialization(Vec<(String, SmeltingExpression)>),
-    StructMemberAccess(Box<SmeltingExpression>, String),
-    Variable(String),
+    Command(ExpressionId, String),
+    FunctionCall(ExpressionId, String, Vec<SmeltingExpression>),
+    ListElementAccess(
+        ExpressionId,
+        Box<SmeltingExpression>,
+        Box<SmeltingExpression>,
+    ),
+    ListInitialization(ExpressionId, Vec<SmeltingExpression>),
+    Literal(ExpressionId, Box<SmeltingLiteral>),
+    Operation(ExpressionId, Box<SmeltingOperation>),
+    StructInitialization(ExpressionId, Vec<(String, SmeltingExpression)>),
+    StructMemberAccess(ExpressionId, Box<SmeltingExpression>, String),
+    Variable(ExpressionId, String),
 }
 
 pub enum SmeltingStatement {
@@ -55,9 +63,70 @@ pub enum SmeltingStatement {
 }
 
 pub struct SmeltingFunction {
+    pub name: String,
+    pub parameters: Vec<String>,
     pub body: Vec<SmeltingStatement>,
 }
 
 pub struct SmeltingProgram {
     pub functions: Vec<SmeltingFunction>,
+}
+
+trait Compile {
+    fn compile(self, builder: &mut DataPackBuilder);
+}
+
+impl Compile for SmeltingExpression {
+    fn compile(self, builder: &mut DataPackBuilder) {
+        match self {
+            SmeltingExpression::Command(id, command) => todo!(),
+            SmeltingExpression::FunctionCall(id, name, arguments) => todo!(),
+            SmeltingExpression::ListElementAccess(id, list, index) => todo!(),
+            SmeltingExpression::ListInitialization(id, elements) => todo!(),
+            SmeltingExpression::Literal(id, literal) => todo!(),
+            SmeltingExpression::Operation(id, operation) => todo!(),
+            SmeltingExpression::StructInitialization(id, members) => todo!(),
+            SmeltingExpression::StructMemberAccess(id, strukt, name) => todo!(),
+            SmeltingExpression::Variable(id, name) => todo!(),
+        }
+    }
+}
+
+impl Compile for SmeltingStatement {
+    fn compile(self, builder: &mut DataPackBuilder) {
+        match self {
+            SmeltingStatement::Assignment(variable, value) => todo!(),
+            SmeltingStatement::Conditional(condition, true_branch, false_branch) => todo!(),
+            SmeltingStatement::Expression(expression) => todo!(),
+            SmeltingStatement::Loop(initialization, condition, update, body) => todo!(),
+            SmeltingStatement::Return(value) => todo!(),
+            SmeltingStatement::Throw(value) => todo!(),
+        }
+    }
+}
+
+impl Compile for SmeltingFunction {
+    fn compile(self, builder: &mut DataPackBuilder) {
+        builder.push_function(self.name);
+
+        for (i, parameter) in self.parameters.iter().enumerate() {
+            builder.push_command(format!("data modify storage smelter:smelter stack[-1].variables.{parameter} set from storage smelter:smelter fnargs[{i}]"));
+        }
+        builder.push_command(format!(
+            "data modify storage smelter:smelter fnargs set value []"
+        ));
+        builder.push_command(format!("data remove storage smelter:smelter fnret"));
+
+        for statement in self.body {
+            statement.compile(builder);
+        }
+    }
+}
+
+impl Compile for SmeltingProgram {
+    fn compile(self, builder: &mut DataPackBuilder) {
+        for function in self.functions {
+            function.compile(builder);
+        }
+    }
 }
